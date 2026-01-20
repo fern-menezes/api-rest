7c7144b0bc241b1f05b49f01690968ad2a23ccac
Fern <fmenezes098@gmail.com>
2026-01-20 15:22:34 -0300

    :sparkles: feat: handle request body data

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,19 @@ import express from "express"
 
 const PORT = 3333
 const app = express()
+app.use(express.json())
 
 app.get("/products", (request, response) => {
     // /products?page=1&limit=10 | parametros nomeados, quan página e limite de páginas(opcionais)
     const { page, limit} = request.query
      response.send(`Pagina ${ page } de ${ limit }`)
+})
+
+app.post("/products", (request, response) =>{
+    const { name, price } = request.body
+    response.send(`Produto ${name} custa $ ${price}`)
+
+    
 })
 
 app.listen(PORT, () => console.log(`Running port ${PORT}...`))
