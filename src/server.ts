24eace64c4bf4757d9ce43425e832149dddd91ad
Fern <fmenezes098@gmail.com>
2026-01-20 14:50:04 -0300

    :sparkles: feat: add route params

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,9 @@ import express from "express"
 const PORT = 3333
 const app = express()
 
+app.get("/products/:id", (request, response) => {
+    const { id } = request.params
+    response.send(`Product ${id}`)
+})
+
 app.listen(PORT, () => console.log(`Running port ${PORT}...`))
