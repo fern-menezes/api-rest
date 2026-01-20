80a66b80e3f6769f443f975f21b6b5a4d1f47083
Fern <fmenezes098@gmail.com>
2026-01-20 15:51:50 -0300

    :sparkles: feat: add local middleware

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,19 @@ import { myMiddleware } from "./middlewares/my-middleware"
 const PORT = 3333
 const app = express()
 app.use(express.json())
-app.use(myMiddleware)
+
+//middleware global, para todas as rotas
+//app.use(myMiddleware)
 
 app.get("/products", (request, response) => {
     // /products?page=1&limit=10 | parametros nomeados, quan página e limite de páginas(opcionais)
-    const { page, limit} = request.query
-     response.send(`Pagina ${ page } de ${ limit }`)
+    const { name, price } = request.query
+     //response.send(`Pagina ${ page } de ${ limit }`)
+
+     response.status(201).json({ name, price })
 })
 
-app.post("/products", (request, response) =>{
+app.post("/products", myMiddleware, (request, response) =>{
     const { name, price } = request.body
     //response.send(`Produto ${name} custa $ ${price}`)
 
