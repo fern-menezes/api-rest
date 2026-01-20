188874af304bb60ad16538d9b0a3f5a3b45fa157
Fern <fmenezes098@gmail.com>
2026-01-20 14:53:35 -0300

    :sparkles: feat: add query params

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import express from "express"
 const PORT = 3333
 const app = express()
 
-app.get("/products/:id", (request, response) => {
-    const { id } = request.params
-    response.send(`Product ${id}`)
+app.get("/products", (request, response) => {
+    // /products?page=1&limit=10 | parametros nomeados, quan página e limite de páginas(opcionais)
+    const { page, limit} = request.query
+     response.send(`Pagina ${ page } de ${ limit }`)
 })
 
 app.listen(PORT, () => console.log(`Running port ${PORT}...`))
