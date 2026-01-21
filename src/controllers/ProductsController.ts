9f28ae4cd033d57563b4f99bd934e8525165af7c
Fern <fmenezes098@gmail.com>
2026-01-21 10:08:08 -0300

    :sparkles: feat: add controller layer

---
+++ src/controllers/ProductsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express"
+
+
+class ProductsController{
+    /**
+     * index - GET para listar vários registros.
+     * show - GET para exibir um registro especifico.
+     * create - POST para criar um registro.
+     * update - PUT para atualizar um registro.
+     * remove - DELETE para |um registro
+     */
+
+    index(request: Request, response:Response){
+        const { page, limit } = request.query
+         response.status(201).json({ page, limit })
+    }
+    create(request: Request, response:Response){
+        const { name, price } = request.body
+        response.status(201).json({ name, price, user_id: request.user_id })
+    }
+}
+export { ProductsController }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -1,20 +1,13 @@
 import { Router } from "express"
 import { myMiddleware } from "../middlewares/my-middleware"
+import { ProductsController } from "../controllers/ProductsController"
 
-export const productsRoutes = Router()
 
-productsRoutes.get("/", (request, response) => {
-    // /products?page=1&limit=10 | parametros nomeados, quan página e limite de páginas(opcionais)
-    const { name, price } = request.query
-     //response.send(`Pagina ${ page } de ${ limit }`)
+const productsController = new ProductsController()
+const productsRoutes = Router()
 
-     response.status(201).json({ name, price })
-})
+productsRoutes.get("/", productsController.index);
+//midleware local em uma rota específica
+productsRoutes.post("/", myMiddleware, productsController.create);
 
-productsRoutes.post("/", myMiddleware, (request, response) =>{
-    const { name, price } = request.body
-    //response.send(`Produto ${name} custa $ ${price}`)
-
-    response.status(201).json({ name, price, user_id: request.user_id })
-
-})
+export { productsRoutes }
